bca0a9a9516f8571c420cb4ae8519a45ba4eded8
Rajnish <b421037@iiit-bh.ac.in>
2023-07-10 11:15:38 +0530

    bookmark button added

--- contentScript.js
+++ contentScript.js
@@ -11,3 +11,23 @@
     }
   });
 };
+const newVideoLoaded = () => {
+  const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+  console.log(bookmarkBtnExists);
+
+  if (!bookmarkBtnExists) {
+    const bookmarkBtn = document.createElement("img");
+
+    bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+    bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+    bookmarkBtn.title = "Click to bookmark current timestamp";
+
+    youtubeLeftControls =
+      document.getElementsByClassName("ytp-left-controls")[0];
+    youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+    youtubeLeftControls.append(bookmarkBtn);
+    bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+  }
+};
+
