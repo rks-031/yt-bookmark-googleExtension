0f3511bf5210fa44bc3f1984df99057b133fe726
Rajnish <b421037@iiit-bh.ac.in>
2023-07-10 12:28:21 +0530

    utils.js finished

--- popup.js
+++ popup.js
@@ -1,23 +1,86 @@
 import { getActiveTabURL } from "./utils.js";
 
-// adding a new bookmark row to the popup
+const addNewBookmark = (bookmarks, bookmark) => {
+  const bookmarkTitleElement = document.createElement("div");
+  const controlsElement = document.createElement("div");
+  const newBookmarkElement = document.createElement("div");
 
-const addNewBookmark = () => {};
+  bookmarkTitleElement.textContent = bookmark.desc;
+  bookmarkTitleElement.className = "bookmark-title";
+  controlsElement.className = "bookmark-controls";
 
-const viewBookmarks = () => {};
+  setBookmarkAttributes("play", onPlay, controlsElement);
+  setBookmarkAttributes("delete", onDelete, controlsElement);
 
-const onPlay = (e) => {};
+  newBookmarkElement.id = "bookmark-" + bookmark.time;
+  newBookmarkElement.className = "bookmark";
+  newBookmarkElement.setAttribute("timestamp", bookmark.time);
 
-const onDelete = (e) => {};
+  newBookmarkElement.appendChild(bookmarkTitleElement);
+  newBookmarkElement.appendChild(controlsElement);
+  bookmarks.appendChild(newBookmarkElement);
+};
 
-const setBookmarkAttributes = () => {};
+const viewBookmarks = (currentBookmarks = []) => {
+  const bookmarksElement = document.getElementById("bookmarks");
+  bookmarksElement.innerHTML = "";
+
+  if (currentBookmarks.length > 0) {
+    for (let i = 0; i < currentBookmarks.length; i++) {
+      const bookmark = currentBookmarks[i];
+      addNewBookmark(bookmarksElement, bookmark);
+    }
+  } else {
+    bookmarksElement.innerHTML = '<i class="row">No bookmarks to show</i>';
+  }
+
+  return;
+};
+
+const onPlay = async (e) => {
+  const bookmarkTime = e.target.parentNode.parentNode.getAttribute("timestamp");
+  const activeTab = await getActiveTabURL();
+
+  chrome.tabs.sendMessage(activeTab.id, {
+    type: "PLAY",
+    value: bookmarkTime,
+  });
+};
+
+const onDelete = async (e) => {
+  const activeTab = await getActiveTabURL();
+  const bookmarkTime = e.target.parentNode.parentNode.getAttribute("timestamp");
+  const bookmarkElementToDelete = document.getElementById(
+    "bookmark-" + bookmarkTime
+  );
+
+  bookmarkElementToDelete.parentNode.removeChild(bookmarkElementToDelete);
+
+  chrome.tabs.sendMessage(
+    activeTab.id,
+    {
+      type: "DELETE",
+      value: bookmarkTime,
+    },
+    viewBookmarks
+  );
+};
+
+const setBookmarkAttributes = (src, eventListener, controlParentElement) => {
+  const controlElement = document.createElement("img");
+
+  controlElement.src = "assets/" + src + ".png";
+  controlElement.title = src;
+  controlElement.addEventListener("click", eventListener);
+  controlParentElement.appendChild(controlElement);
+};
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const activeTab = getActiveTabURL();
-  const queryParams = activeTab.url.split("?")[1];
-  const urlParams = new URLSearchParams(queryParams);
+  const activeTab = await getActiveTabURL();
+  const queryParameters = activeTab.url.split("?")[1];
+  const urlParameters = new URLSearchParams(queryParameters);
 
-  const currentVideo = urlParams.get("v");
+  const currentVideo = urlParameters.get("v");
 
   if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
     chrome.storage.sync.get([currentVideo], (data) => {
@@ -25,12 +88,12 @@ document.addEventListener("DOMContentLoaded", async () => {
         ? JSON.parse(data[currentVideo])
         : [];
 
-      //viewBookmarks
+      viewBookmarks(currentVideoBookmarks);
     });
   } else {
     const container = document.getElementsByClassName("container")[0];
 
     container.innerHTML =
-      "<div class='title'>This is not a youtube video page.</div>";
+      '<div class="title">This is not a youtube video page.</div>';
   }
 });
--- utils.js
+++ utils.js
@@ -1,7 +1,10 @@
 //this helps us to find the active tab that the user is on and whether the user has already created a bookmark on this page.
 
 export async function getActiveTabURL() {
-  let queryOptions = { active: true, currentWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
+  const tabs = await chrome.tabs.query({
+    currentWindow: true,
+    active: true,
+  });
+
+  return tabs[0];
 }
